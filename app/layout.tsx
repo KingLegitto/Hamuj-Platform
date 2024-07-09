512e79bd6ce8f42197d22d3923676473551fd9e3
Nikechukwum <kinglegitto@gmail.com>
2024-07-09 18:32:06 +0100

    Feat: added fav icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,17 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
+import Head from "next/head";
+
 
 const inter = Inter({ subsets: ["latin"], weight: ['300','400', '700', '800'] });
 
 export const metadata: Metadata = {
   title: "Hamuj Homes",
-  description: "Your go to option for interior designs and construction.",
+  description: "Your go to option for interior renovations, furniture and construction.",
+  icons: {
+    icon: '/favicon.png'
+  }
 };
 
 export default function RootLayout({
@@ -17,6 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <body className={inter.className}>
         <Navbar />
         {children}
