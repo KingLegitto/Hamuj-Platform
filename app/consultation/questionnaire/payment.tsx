4c17097d43d17eff8bffd711fc9eff6ee680fb4d
Nikechukwum <kinglegitto@gmail.com>
2024-08-15 11:30:42 +0100

    Updates

--- app/consultation/questionnaire/payment.tsx
+++ app/consultation/questionnaire/payment.tsx
@@ -1,5 +1,7 @@
 "use client"
-import { FC, useEffect } from "react";
+import Toast from "@/components/toast";
+import { AnimatePresence } from "framer-motion";
+import { FC, useEffect, useState } from "react";
 import { PaystackButton } from "react-paystack";
 
 interface PaymentProps {
@@ -7,6 +9,8 @@ interface PaymentProps {
     setPage: (page: any)=>void
 }
 const Payment: FC<PaymentProps> = ({ email, setPage }) => {
+  const [toast, setToast] = useState<boolean>(false)
+  const [toastDetails, setToastDetails] = useState({title: '', result: false, message: ''})
   const paymentConfig = {
     email: email,
     amount: 70000*100,
@@ -14,7 +18,10 @@ const Payment: FC<PaymentProps> = ({ email, setPage }) => {
     text: "Pay Now",
     onSuccess: () =>
       setPage('sending'),
-    onClose: () => alert("Wait! Don't leave :("),
+    onClose: () => (
+      setToast(true),
+    setToastDetails({title: 'Unsuccessful', result: false, message: 'Transaction terminated' })
+  ),
   };
 
   useEffect(() => {
@@ -24,6 +31,9 @@ const Payment: FC<PaymentProps> = ({ email, setPage }) => {
 
   return (
     <section className="target relative mx-auto bg-white border-[3px] lg:border-[5px] border-white text-sm md:text-base flex flex-col justify-center items-center gap-y-12 rounded-3xl text-grade-3 p-7 py-20 lg:p-16 w-[97%] md:w-[70%] lg:h-[70vh] max-h-[500px] shadow-[inset_5px_5px_0px_6px_#f3f3f3] md:shadow-[inset_9px_9px_0px_10px_#f3f3f3] text-center my-[5rem] md:my-[10rem]">
+      <AnimatePresence>
+            {toast && <Toast setToast={setToast} toastDetails={toastDetails}/>}
+      </AnimatePresence>
       <h2 className="relative font-normal text-lg md:text-xl text-grade-3">
         Proceed with payment
         <div className="h-[3px] lg:h-[5px] w-full lg:w-[150%] absolute bg-[#f3f3f3] -bottom-3 left-1/2 -translate-x-1/2" />
